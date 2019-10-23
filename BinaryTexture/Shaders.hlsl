2bef76a41d9d3a75a0a7ec09e5f7faab59f88ea7
PickNose <48274192+AhnaGeneral@users.noreply.github.com>
2019-10-24 01:22:53 +0900

    Finish a Texture
    Modify the example framework to finish texturing

---
+++ BinaryTexture/Shaders.hlsl
@@ -0,0 +1,79 @@
+struct MATERIAL
+{
+	float4					m_cAmbient;
+	float4					m_cDiffuse;
+	float4					m_cSpecular; //a = power
+	float4					m_cEmissive;
+};
+
+cbuffer cbCameraInfo : register(b1)
+{
+	matrix					gmtxView : packoffset(c0);
+	matrix					gmtxProjection : packoffset(c4);
+	float3					gvCameraPosition : packoffset(c8);
+};
+
+cbuffer cbGameObjectInfo : register(b2)
+{
+	matrix					gmtxGameObject : packoffset(c0);
+	MATERIAL				gMaterial : packoffset(c4);
+};
+
+#include "Light.hlsl"
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+
+Texture2D gtxtTexture : register(t0);
+SamplerState gSamplerState : register(s0);
+//#define _WITH_VERTEX_LIGHTING
+
+struct VS_LIGHTING_INPUT
+{
+	float3 position : POSITION;
+	float3 normal : NORMAL;
+	float2 uv : TEXCOORD;
+};
+
+struct VS_LIGHTING_OUTPUT
+{
+	float4 position : SV_POSITION;
+	float3 positionW : POSITION;
+	float3 normalW : NORMAL;
+	float2 uv : TEXCOORD;
+#ifdef _WITH_VERTEX_LIGHTING
+	float4 color : COLOR;
+#endif
+};
+
+VS_LIGHTING_OUTPUT VSLighting(VS_LIGHTING_INPUT input)
+{
+	VS_LIGHTING_OUTPUT output;
+
+	output.normalW = mul(input.normal, (float3x3)gmtxGameObject);
+	output.positionW = (float3)mul(float4(input.position, 1.0f), gmtxGameObject);
+	output.position = mul(mul(float4(output.positionW, 1.0f), gmtxView), gmtxProjection);
+	output.uv = input.uv; 
+
+#ifdef _WITH_VERTEX_LIGHTING
+	output.normalW = normalize(output.normalW);
+	output.color = Lighting(output.positionW, output.normalW);
+#endif
+	return(output);
+}
+
+float4 PSLighting(VS_LIGHTING_OUTPUT input) : SV_TARGET
+{
+#ifdef _WITH_VERTEX_LIGHTING
+	return(input.color);
+#else
+	
+	input.normalW = normalize(input.normalW);
+	float4 color = Lighting(input.positionW, input.normalW);
+	float4 cColor = gtxtTexture.Sample(gSamplerState, input.uv);
+	
+	cColor = lerp(color, cColor, 0.5f);
+	return(cColor);
+#endif
+}
+
